36832113cbc0b0fd084c8eaa1891f7b234319c6c
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-08-23 21:49:03 -0500

    Update colorGame00.js

--- colorGame00.js
+++ colorGame00.js
@@ -1,14 +1,22 @@
-const Board = () => {
-  const [player, setPlayer] = React.useState(0);
-  let status = `Player ${player}`;
+const Square = ({id}) => {
   return (
-    <div
-      className="game-board"
-      onClick={(e) => {
-        setPlayer(player + 1);
-        status = `Player ${player}`;
-      }}
-    >
+    
+    <button> <h1>{id}</h1></button>
+  )
+}
+
+
+const Board = () => {
+  const [player, setPlayer] = React.useState(1);
+  let status = `Player ${player}`;
+  function renderSquare(i){
+    return (<Square id={i}> </Square>)
+  }
+  return (
+    <div className="game-board">
+      <div className="grid-row">
+        {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
+      </div>
       <div id="info">
         <h1>{status}</h1>
       </div>
